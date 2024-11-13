890c0450d39a9eb5b3733a975f6864af0fab7af1
latesoon <18722695567@163.com>
2024-11-13 21:09:05 +0800

    lru

--- lab3/code/lru/kern/mm/swap_lru.c
+++ lab3/code/lru/kern/mm/swap_lru.c
@@ -1,9 +1,9 @@
-﻿﻿#include <defs.h>
+#include <defs.h>
 #include <riscv.h>
 #include <stdio.h>
 #include <string.h>
 #include <swap.h>
-#include <swap_lru.h>
+#include <swap_fifo.h>
 #include <list.h>
 
 
@@ -14,46 +14,24 @@ _lru_init_mm(struct mm_struct* mm)
 {
     list_init(&pra_list_head);
     mm->sm_priv = &pra_list_head;
-
+  
     return 0;
 }
 
 static int
-_lru_tick_event(struct mm_struct* mm)
+_lru_map_swappable(struct mm_struct *mm, uintptr_t addr, struct Page *page, int swap_in)
 {
-    list_entry_t* head = (list_entry_t*)mm->sm_priv;  //head指向哨兵节点
-    assert(head != NULL);  //֤确认head被初始化
-    list_entry_t* entry = list_next(head); //第一节点
-    while (entry != head)
-    {
-        struct Page* page = le2page(entry, pra_page_link);//entry指向页面
-        pte_t* ptep = get_pte(mm->pgdir, page->pra_vaddr, 0);  //entry页面页表项
-        if (*ptep & PTE_A)//判断A位是否为1
-        {
-            list_del(entry);
-            list_add(head, entry);
-            *ptep &= ~PTE_A;  //A换成0
-            tlb_invalidate(mm->pgdir, page->pra_vaddr);
-        }
-        entry = list_prev(head);
-    }
-    cprintf("_lru_tick_event is called!\n");
-
-    return 0;
-}
-
-static int
-_lru_map_swappable(struct mm_struct* mm, uintptr_t addr, struct Page* page, int swap_in)
-{
-
-    list_entry_t* head = (list_entry_t*)mm->sm_priv;
-    list_entry_t* entry = &(page->pra_page_link);
-
+    list_entry_t *head=(list_entry_t*) mm->sm_priv;
+    list_entry_t *entry=&(page->pra_page_link);
+ 
     assert(entry != NULL && head != NULL);
+    //record the page access situlation
+
+    //(1)link the most recent arrival page at the back of the pra_list_head qeueue.
     list_add(head, entry);
+    page->visited=1;
     return 0;
 }
-
 static int
 _lru_swap_out_victim(struct mm_struct* mm, struct Page** ptr_page, int in_tick)
 {
@@ -72,48 +50,31 @@ _lru_swap_out_victim(struct mm_struct* mm, struct Page** ptr_page, int in_tick)
 }
 
 static int
-_lru_check_swap(void)
-{
+_lru_check_swap(struct mm_struct* mm) {
     cprintf("write Virt Page c in lru_check_swap\n");
     *(unsigned char*)0x3000 = 0x0c;
-    
+    swap_tick_event(mm);
     assert(pgfault_num == 4);
     cprintf("write Virt Page a in lru_check_swap\n");
     *(unsigned char*)0x1000 = 0x0a;
-  
+    swap_tick_event(mm);
     assert(pgfault_num == 4);
     cprintf("write Virt Page d in lru_check_swap\n");
     *(unsigned char*)0x4000 = 0x0d;
-
+    swap_tick_event(mm);
     assert(pgfault_num == 4);
     cprintf("write Virt Page b in lru_check_swap\n");
     *(unsigned char*)0x2000 = 0x0b;
+    swap_tick_event(mm);
     assert(pgfault_num == 4);
     cprintf("write Virt Page e in lru_check_swap\n");
     *(unsigned char*)0x5000 = 0x0e;
+    swap_tick_event(mm);
     assert(pgfault_num == 5);
-    cprintf("write Virt Page b in lru_check_swap\n");
-    *(unsigned char*)0x2000 = 0x0b;
-    assert(pgfault_num == 5);
-    cprintf("write Virt Page a in lru_check_swap\n");
-    *(unsigned char*)0x1000 = 0x0a;
+    cprintf("write Virt Page c in lru_check_swap\n");
+    *(unsigned char*)0x4000 = 0x0c;
+    swap_tick_event(mm);
     assert(pgfault_num == 6);
-    cprintf("write Virt Page b in lru_check_swap\n");
-    *(unsigned char*)0x2000 = 0x0b;
-    assert(pgfault_num == 7);
-    cprintf("write Virt Page c in lru_check_swap\n");
-    *(unsigned char*)0x3000 = 0x0c;
-    assert(pgfault_num == 8);
-    cprintf("write Virt Page d in lru_check_swap\n");
-    *(unsigned char*)0x4000 = 0x0d;
-    assert(pgfault_num == 9);
-    cprintf("write Virt Page e in lru_check_swap\n");
-    *(unsigned char*)0x5000 = 0x0e;
-    assert(pgfault_num == 10);
-    cprintf("write Virt Page a in lru_check_swap\n");
-    assert(*(unsigned char*)0x1000 == 0x0a);
-    *(unsigned char*)0x1000 = 0x0a;
-    assert(pgfault_num == 11);
     return 0;
 }
 
@@ -127,6 +88,29 @@ _lru_init(void)
 static int
 _lru_set_unswappable(struct mm_struct* mm, uintptr_t addr)
 {
+    return 0;
+}
+
+static int
+_lru_tick_event(struct mm_struct* mm)
+{
+    cprintf("_lru_tick_event is called!\n");
+    list_entry_t* head = (list_entry_t*)mm->sm_priv;
+    assert(head != NULL);
+    list_entry_t* entry = list_next(head);
+    while (entry != head)
+    {
+        struct Page* page = le2page(entry, pra_page_link);
+        if (page->visited)
+        {
+            //cprintf("111");
+            list_del(entry);
+            list_add(head, entry);
+            page->visited=0;
+        }
+        entry = list_next(entry);
+    }
+    cprintf("_lru_tick_event is finished!\n");
     return 0;
 }
 
