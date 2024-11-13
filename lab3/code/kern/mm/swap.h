3ee5578259928a55f709cd4dd72bacea15db2fce
F1nt233 <2059677557@qq.com>
2024-11-13 18:39:44 +0800

    Update swap.h

--- lab3/code/kern/mm/swap.h
+++ lab3/code/kern/mm/swap.h
@@ -47,7 +47,7 @@ struct swap_manager
      /* Try to swap out a page, return then victim */
      int (*swap_out_victim) (struct mm_struct *mm, struct Page **ptr_page, int in_tick);
      /* check the page relpacement algorithm */
-     int (*check_swap)(struct mm_struct* mm);
+     int (*check_swap)(void);
 };
 
 extern volatile int swap_init_ok;
